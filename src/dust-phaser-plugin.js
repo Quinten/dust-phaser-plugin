00501ca178239043789dbc7908cff1e70399b3f7
Quinten Clause <quinten.clause@studioemma.eu>
2019-03-10 14:02:17 +0100

    Add 3 parameters count, alpha and tint

--- src/dust-phaser-plugin.js
+++ src/dust-phaser-plugin.js
@@ -15,23 +15,33 @@ class DustPhaserPlugin extends Phaser.Plugins.ScenePlugin
         }
     }
 
-    addOnePixelDust()
+    addOnePixelDust({
+        count = 8,
+        alpha = 1,
+        tint = 0xffffff
+    } = {})
     {
         if (!this.systems.textures.exists("dust")) {
             this.systems.textures.once('addtexture', () => {
-                this.addDust();
+                this.addDust({count, alpha, tint});
             });
             this.systems.textures.addBase64("dust", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=");
         } else {
-            this.addDust();
+            this.addDust({count, alpha, tint});
         }
     }
 
-    addDust()
+    addDust({
+        count = 8,
+        alpha = 1,
+        tint = 0xffffff
+    } = {})
     {
         let dusts = [];
-        for (let d = 0; d < 8; d++) {
+        for (let d = 0; d < count; d++) {
             let dust = new DustSprite(this.scene, Math.floor(Math.random() * this.scene.cameras.main.width), Math.floor(Math.random() * this.scene.cameras.main.height));
+            dust.alpha = alpha;
+            dust.tint = tint;
             dusts.push(dust);
         }
         let container = this.scene.add.container(0, 0, dusts);
