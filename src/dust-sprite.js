3b6551de3c9dc3334ab90ec1cb6963054702bbc5
Quinten Clause <quintenclause@hotmail.com>
2022-12-25 12:57:12 +0100

    Update dust-sprite.js
    Consider timeDelta

--- src/dust-sprite.js
+++ src/dust-sprite.js
@@ -14,41 +14,50 @@ class DustSprite extends Phaser.GameObjects.Image
         this.behavior = {};
         this.behavior.position = new Phaser.Math.Vector2(x, y);
         this.behavior.steeringForce = new Phaser.Math.Vector2(0, 0);
+        this.behavior.velocity = new Phaser.Math.Vector2(0, 0);
+        this.behavior.wanderAngle = 0;
+
         this.behavior.maxForce = 1;
         this.behavior.mass = 1;
-        this.behavior.velocity = new Phaser.Math.Vector2(0, 0);
         this.behavior.maxSpeed = 1;
         this.behavior.wanderDistance = 10;
         this.behavior.wanderRadius = 5;
-        this.behavior.wanderAngle = 0;
         this.behavior.wanderRange = 0.5;
+
+        this.behavior.consts = ["maxForce", "mass", "maxSpeed", "wanderDistance", "wanderRadius", "wanderRange"];
+
         this.behavior.cameraPosition = this.scene.cameras.main.midPoint.clone();
     }
 
     update(time, deltaTime)
     {
+        let factor = deltaTime / 17;
         let _ = this.behavior;
-        let center = _.velocity.clone().normalize().multiply(new Phaser.Math.Vector2(_.wanderDistance, _.wanderDistance));
+        let f = {...this.behavior};
+        for (let key in f.consts) {
+            f[key] = f[key] * factor;
+        }
+        let center = _.velocity.clone().normalize().multiply(new Phaser.Math.Vector2(f.wanderDistance, f.wanderDistance));
         let offset = new Phaser.Math.Vector2(1, 0);
-        offset.normalize().multiply(new Phaser.Math.Vector2(_.wanderRadius, _.wanderRadius));
+        offset.normalize().multiply(new Phaser.Math.Vector2(f.wanderRadius, f.wanderRadius));
         let len = offset.length();
         offset.x = Math.cos(_.wanderAngle) * len;
         offset.y = Math.sin(_.wanderAngle) * len;
-        _.wanderAngle += Math.random() * _.wanderRange - _.wanderRange * .5;
+        _.wanderAngle += Math.random() * f.wanderRange - f.wanderRange * .5;
         let force = center.add(offset);
         _.steeringForce = _.steeringForce.add(force);
 
         // steering
-        if (_.steeringForce.lengthSq() > _.maxForce * _.maxForce) {
-            _.steeringForce.normalize().multiply(new Phaser.Math.Vector2(_.maxForce, _.maxForce));
+        if (_.steeringForce.lengthSq() > f.maxForce * f.maxForce) {
+            _.steeringForce.normalize().multiply(new Phaser.Math.Vector2(f.maxForce, f.maxForce));
         }
-        _.steeringForce = _.steeringForce.divide(new Phaser.Math.Vector2(_.mass, _.mass));
+        _.steeringForce = _.steeringForce.divide(new Phaser.Math.Vector2(f.mass, f.mass));
         _.velocity = _.velocity.add(_.steeringForce);
         _.steeringForce = new Phaser.Math.Vector2(0, 0);
 
         // velocity
-        if (_.velocity.lengthSq() > _.maxSpeed * _.maxSpeed) {
-            _.velocity.normalize().multiply(new Phaser.Math.Vector2(_.maxSpeed, _.maxSpeed));
+        if (_.velocity.lengthSq() > f.maxSpeed * f.maxSpeed) {
+            _.velocity.normalize().multiply(new Phaser.Math.Vector2(f.maxSpeed, f.maxSpeed));
         }
         _.position = _.position.add(_.velocity);
 
